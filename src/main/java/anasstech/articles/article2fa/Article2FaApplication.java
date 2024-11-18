a6e0ada74d9b6dbdf8a21f0ac9462d7e6247024b
anass <adakhouch@ribatis.com>
2024-11-18 21:59:41 +0100

    first commit

---
+++ src/main/java/anasstech/articles/article2fa/Article2FaApplication.java
@@ -0,0 +1,13 @@
+package anasstech.articles.article2fa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Article2FaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Article2FaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/anasstech/articles/article2fa/Article2FaApplicationTests.java
@@ -0,0 +1,13 @@
+package anasstech.articles.article2fa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Article2FaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
